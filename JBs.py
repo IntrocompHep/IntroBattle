7c854b94c6e35bca30ea2bd2ad6d14bc9fb9b02b
Introcomp - JBs <147340071+IntrocompHep@users.noreply.github.com>
2023-10-12 17:43:14 -0300

    Update JBs.py

--- JBs.py
+++ JBs.py
@@ -1,14 +1,25 @@
-# todos os import necessários
 import pygame
 
 # setup de variáveis obrigatórias
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1024, 768))
+pygame.display.set_caption('Battle')
 clock = pygame.time.Clock()
 running = True
 
+#baixar imagens
+#imagens de fundo
+background_img = pygame.image.load('c:\\Users\\joaom\Downloads\\teste3ps.png').convert_alpha()
+background_img = pygame.transform.scale(background_img,(1024,768))
+#função pra desenhar o fundo
+def draw_bg():
+    screen.blit(background_img, (0,0))
+
 def main():
+    global running;
     while running:
+        #desenhar fundo
+        draw_bg()
         # Área para identficação de eventos
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
